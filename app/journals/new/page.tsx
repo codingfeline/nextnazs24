66692a820795b02ae106cf9d441ecf0a40adff82
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-10 05:28:20 +0000

    add markdown

--- app/journals/new/page.tsx
+++ app/journals/new/page.tsx
@@ -1,11 +1,14 @@
-import { Button, Heading, TextArea, TextField } from '@radix-ui/themes'
+'use client'
+import { Button, Heading, TextField } from '@radix-ui/themes'
+import 'easymde/dist/easymde.min.css'
+import SimpleMDE from 'react-simplemde-editor'
 
 const NewJournalPage = () => {
   return (
     <div className="max-w-xl space-y-3">
-      <Heading>NewJournalPage</Heading>
+      <Heading>New Journal</Heading>
       <TextField.Root placeholder="Title" />
-      <TextArea placeholder="Story" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit Story</Button>
     </div>
   )
