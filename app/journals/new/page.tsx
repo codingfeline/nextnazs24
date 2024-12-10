b7a0e06255ef0cc829e0632a538b6e0508407302
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-10 05:50:49 +0000

    add react-hook-form (no api route yet)

--- app/journals/new/page.tsx
+++ app/journals/new/page.tsx
@@ -1,16 +1,32 @@
 'use client'
 import { Button, Heading, TextField } from '@radix-ui/themes'
 import 'easymde/dist/easymde.min.css'
+import { Controller, useForm } from 'react-hook-form'
 import SimpleMDE from 'react-simplemde-editor'
 
+interface JournalForm {
+  title: string
+  description: string
+}
+
 const NewJournalPage = () => {
+  const { register, control, handleSubmit } = useForm<JournalForm>()
+  console.log(register('title'))
+
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(data => console.log(data))}
+    >
       <Heading>New Journal</Heading>
-      <TextField.Root placeholder="Title" />
-      <SimpleMDE placeholder="Description" />
+      <TextField.Root placeholder="Title" {...register('title')} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+      />
       <Button>Submit Story</Button>
-    </div>
+    </form>
   )
 }
 
