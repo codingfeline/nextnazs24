bc5f10152bdf0e901955aec668411dc782acc8ac
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-14 07:57:33 +0000

    refactor - organise imports

---
+++ app/components/index.ts
@@ -0,0 +1,5 @@
+export { default as MyButton } from './Button'
+export { default as Link } from './Link'
+export { default as Skeleton } from './Skeleton'
+export { default as Spinner } from './Spinner'
+
