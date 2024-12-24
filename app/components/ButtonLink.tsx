060b8e72c7c1bd6fbcfeba181acbe3b8cacd0a8e
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-24 06:07:34 +0000

    add dynamic w-full

--- app/components/ButtonLink.tsx
+++ app/components/ButtonLink.tsx
@@ -8,15 +8,16 @@ interface Prop {
   href: string
   Icon?: ComponentType<IconProps>
   children: ReactNode
+  full?: string
 }
 
-const ButtonWithComponent = async ({ href, Icon, children }: Prop) => {
+const ButtonWithComponent = async ({ href, Icon, children, full }: Prop) => {
   const session = await getServerSession(authOptions)
   if (!session) return null
 
   return (
     <Link href={href}>
-      <Button className="my-1 cursor-pointer mr-1">
+      <Button className={`my-1 cursor-pointer mr-1 ${full}`}>
         <div className="flex items-center gap-1">
           {Icon && <Icon />} {children}
         </div>
