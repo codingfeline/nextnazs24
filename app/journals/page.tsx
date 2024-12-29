60913b710ef515991db644f1d7567208f5aa5018
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-29 23:26:52 +0000

    remove content from preview

--- app/journals/page.tsx
+++ app/journals/page.tsx
@@ -1,8 +1,9 @@
 import prisma from '@/prisma/client'
 import { Table } from '@radix-ui/themes'
 // import parse from 'html-react-parser'
+
+import ButtonWithComponent from '../components/ButtonLink'
 import Link from '../components/Link'
-import JournalActions from './journalActions'
 
 const JournalsPage = async () => {
   const journals = await prisma.journals.findMany()
@@ -10,14 +11,14 @@ const JournalsPage = async () => {
 
   return (
     <div className="">
-      <JournalActions />
+      <ButtonWithComponent href="/journals/new">New Journal2</ButtonWithComponent>
       <Table.Root variant="surface" className=" bg-red-50">
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell className="hidden md:table-cell">
               Topic
             </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Journal</Table.ColumnHeaderCell>
+            {/* <Table.ColumnHeaderCell>Journal</Table.ColumnHeaderCell> */}
             <Table.ColumnHeaderCell className="hidden md:table-cell">
               Date
             </Table.ColumnHeaderCell>
@@ -30,13 +31,14 @@ const JournalsPage = async () => {
                 <Table.Cell className="">
                   <Link href={`/journals/${j.id}`}>{j.topic}</Link>
                 </Table.Cell>
-                <Table.Cell className="hidden md:table-cell w-full">
+                {/* <Table.Cell className="hidden md:table-cell w-full">
                   {j.comment}
-                </Table.Cell>
-                <Table.Cell className="hidden md:table-cell md:w-[150px]">
-                  {j.date.toDateString()}
+                </Table.Cell> */}
+                <Table.Cell className="hidden sm:table-cell sm:w-[150px]">
+                  {j.date.toLocaleString()}
+                  {/* {j.date.toDateString()}
                   <br /> {j.date.getHours()}
-                  {j.date.getMinutes()} hrs
+                  {j.date.getMinutes()} hrs */}
                 </Table.Cell>
               </Table.Row>
             )
