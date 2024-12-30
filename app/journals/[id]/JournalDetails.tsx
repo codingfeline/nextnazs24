1172087e79022dce621c3ed7d67f5c31e7021df6
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-30 23:24:38 +0000

    cosm text colour

--- app/journals/[id]/JournalDetails.tsx
+++ app/journals/[id]/JournalDetails.tsx
@@ -5,8 +5,8 @@ import ReactMarkDown from 'react-markdown'
 const JournalDetails = ({ journal }: { journal: Journals }) => {
   return (
     <>
-      <Heading>{journal.topic}</Heading>
-      <Card className="prose flex flex-col w-full " mt="4">
+      <Heading className="text-white mt-3">{journal.topic}</Heading>
+      <Card className="prose flex flex-col w-full bg-gray-100 " mt="4">
         <ReactMarkDown>{journal.comment}</ReactMarkDown>
       </Card>
       <div className="flex justify-end p-1">
