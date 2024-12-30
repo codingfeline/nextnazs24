3547ee2a16bb44e398fe67da2f8e1556a2d95ab8
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-30 14:45:13 +0000

    refactor

--- app/journals/page.tsx
+++ app/journals/page.tsx
@@ -1,9 +1,9 @@
 import prisma from '@/prisma/client'
-import { Table } from '@radix-ui/themes'
+import { Card, Flex, Text } from '@radix-ui/themes'
 // import parse from 'html-react-parser'
 
+import { Link } from '../components'
 import ButtonWithComponent from '../components/ButtonLink'
-import Link from '../components/Link'
 
 const JournalsPage = async () => {
   const journals = await prisma.journals.findMany()
@@ -12,39 +12,17 @@ const JournalsPage = async () => {
   return (
     <div className="">
       <ButtonWithComponent href="/journals/new">New Journal2</ButtonWithComponent>
-      <Table.Root variant="surface" className=" bg-red-50">
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Topic
-            </Table.ColumnHeaderCell>
-            {/* <Table.ColumnHeaderCell>Journal</Table.ColumnHeaderCell> */}
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Date
-            </Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {journals.map(j => {
-            return (
-              <Table.Row key={j.id}>
-                <Table.Cell className="">
-                  <Link href={`/journals/${j.id}`}>{j.topic}</Link>
-                </Table.Cell>
-                {/* <Table.Cell className="hidden md:table-cell w-full">
-                  {j.comment}
-                </Table.Cell> */}
-                <Table.Cell className="hidden sm:table-cell sm:w-[150px]">
-                  {j.date.toLocaleString()}
-                  {/* {j.date.toDateString()}
-                  <br /> {j.date.getHours()}
-                  {j.date.getMinutes()} hrs */}
-                </Table.Cell>
-              </Table.Row>
-            )
-          })}
-        </Table.Body>
-      </Table.Root>
+
+      {journals.map(j => {
+        return (
+          <Card key={j.id} className="gap-2 mb-1">
+            <Flex justify="between">
+              <Link href={`/journals/${j.id}`}>{j.topic}</Link>
+              <Text>{j.date.toLocaleString()}</Text>
+            </Flex>
+          </Card>
+        )
+      })}
     </div>
   )
 }
