7784296619fc253cef7bc1ebe7c5e16e0047cb04
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-30 21:33:21 +0000

    refactor ShowTopics

--- app/journals/_components/filterJournals.tsx
+++ app/journals/_components/filterJournals.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import ButtonIcon from '@/app/components/ButtonIcon'
-import { Box, Flex, TextField } from '@radix-ui/themes'
+import { Box, Container, Flex, TextField } from '@radix-ui/themes'
 import { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 
@@ -20,34 +20,38 @@ const FilterJournals = ({ topics }: Prop) => {
   const filtered = topics.filter(t =>
     t.topic.toLowerCase().includes(searchTerm.toLowerCase())
   )
-  return (
-    <>
-      <Flex my="2">
-        <Box width="5">
-          <TextField.Root
-            placeholder="find journals…"
-            onChange={e => {
-              setSearchTerm(e.target.value)
-            }}
-          >
-            <TextField.Slot>
-              <FaSearch height="16" width="16" />
-            </TextField.Slot>
-          </TextField.Root>
-        </Box>
-      </Flex>
 
-      {searchTerm !== '' &&
-        filtered.map(t => {
-          return (
-            <div key={t.id}>
-              <Flex>
-                <ButtonIcon href={`/journals/${t.id}`}>{t.topic}</ButtonIcon>
-              </Flex>
-            </div>
-          )
-        })}
-    </>
+  const ShowTopics = () =>
+    filtered.map(t => {
+      return (
+        <div key={t.id} className="w-ful ">
+          <Flex my="1" wrap="nowrap">
+            <ButtonIcon href={`/journals/${t.id}`}>{t.topic}</ButtonIcon>
+          </Flex>
+        </div>
+      )
+    })
+
+  return (
+    <div className=" ">
+      <Flex my="2" className="place-self-center py-1" direction="column">
+        <Container size="3">
+          <Box width="5" className="max-w-screen-lg">
+            <TextField.Root
+              placeholder="find journals…"
+              onChange={e => {
+                setSearchTerm(e.target.value)
+              }}
+            >
+              <TextField.Slot>
+                <FaSearch height="16" width="16" />
+              </TextField.Slot>
+            </TextField.Root>
+          </Box>
+          {searchTerm !== '' && <ShowTopics />}
+        </Container>
+      </Flex>
+    </div>
   )
 }
 
