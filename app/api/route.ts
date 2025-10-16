32ef7c6b9dd38d72551def10b6ad61cfd530f22a
Nazie Sharid <nazie.sh4rid@gmail.com>
2025-10-16 08:53:42 +0100

    password

---
+++ app/api/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server"
+
+export async function GET() {
+
+  // function generatePassword(len=15) {
+  let password = ''
+  let randStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"
+
+  for (let i = 0; i < 16; i++) {
+    // console.log(i)
+    let n = Math.floor(Math.random() * randStr.length)
+    password += randStr[n]
+  }
+
+  // return password
+  // }
+  return NextResponse.json(password)
+}
