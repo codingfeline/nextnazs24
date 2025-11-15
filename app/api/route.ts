e9cc4ac6d95f0210554429d8c23de92feca28c60
Nazie Sharid <nazie.sh4rid@gmail.com>
2025-11-15 16:01:27 +0000

    pw gen

---
+++ app/api/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(request: NextRequest) {
+
+  const searchParams = request.nextUrl.searchParams
+  const lengthString = searchParams.get('length')
+  let passLength: number | null = null
+
+  if (lengthString) {
+    const parsedLength = parseInt(lengthString, 10)
+    if (!isNaN(parsedLength)) {
+      passLength = parsedLength
+    }
+  }
+
+  if (passLength === null || passLength < 1) {
+    passLength = 10
+  }
+
+  let password = ''
+  const randStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()<>"
+
+  for (let i = 0; i < passLength; i++) {
+    // console.log(i)
+    const n = Math.floor(Math.random() * randStr.length)
+    password += randStr[n]
+  }
+
+  return new Response(password + '\n', {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain'
+    }
+  })
+  return NextResponse.json(password)
+}
