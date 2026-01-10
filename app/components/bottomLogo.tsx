d937c926bd004191db0586f86de9cf0bba273857
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-01-10 17:33:38 +0000

    fix Vercel issue

--- app/components/bottomLogo.tsx
+++ app/components/bottomLogo.tsx
@@ -12,7 +12,7 @@ export default function BottomLogo() {
     <div>
       <motion.form initial={{ y: -520 }} animate={{ y: 0, x: -3 }}>
         <div className="opacity-30">
-          <div className="absolute bg_logo h-20 w-20 bottom-4 right-1 bg-opacity-10 "></div>
+          <div className="absolute bg-cover bg-[url(/logo.webp)] h-20 w-20 bottom-4 right-1 bg-opacity-10 "></div>
         </div>
       </motion.form>
     </div>
