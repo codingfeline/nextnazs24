381d3d67e3bc3108a69a7faae5370918dc2048a2
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-01-10 14:26:29 +0000

    cosm framer

--- app/components/bottomLogo.tsx
+++ app/components/bottomLogo.tsx
@@ -9,8 +9,10 @@ export default function BottomLogo() {
   if (path !== '/contact') return null
 
   return (
-    <motion.form initial={{ y: -520 }} animate={{ y: 0 }}>
+    <div>
       <div className=" bg-cover w-20 h-20 absolute text-white bottom-20 right-4 bg-[url(/nazsnet.png)]"></div>
-    </motion.form>
+
+      <motion.form initial={{ y: -520 }} animate={{ y: 0 }}></motion.form>
+    </div>
   )
 }
