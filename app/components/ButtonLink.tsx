0536821094995f53601157fe0b9cc9c5dbf3b212
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-01-22 06:18:30 +0000

    cosm mb-1

--- app/components/ButtonLink.tsx
+++ app/components/ButtonLink.tsx
@@ -1,8 +1,8 @@
 import { Button, IconProps } from '@radix-ui/themes'
-import { getServerSession } from 'next-auth'
+// import { getServerSession } from 'next-auth'
 import Link from 'next/link'
 import { ComponentType, ReactNode } from 'react'
-import { authOptions } from '../api/auth/[...nextauth]/authOptions'
+// import { authOptions } from '../api/auth/[...nextauth]/authOptions'
 
 interface Prop {
   href: string
@@ -13,14 +13,14 @@ interface Prop {
 }
 
 const ButtonWithComponent = async ({ href, Icon, children, full, margin }: Prop) => {
-  const session = await getServerSession(authOptions)
-  if (!session) return null
-  if (session!.user!.email !== 'post@nazs.net') return null
+  // const session = await getServerSession(authOptions)
+  // if (!session) return null
+  // if (session!.user!.email !== 'post@nazs.net') return null
 
   return (
     <div>
       <Link href={href}>
-        <Button className={`my-2 cursor-pointer mr-1 ${full} ? 'w-full' :'' ${margin}`}>
+        <Button className={`my-2 cursor-pointer mr-1 ${full ? 'w-full' : ''} ${margin}`}>
           <div className="flex items-center gap-2">
             {Icon && <Icon />} {children}
           </div>
