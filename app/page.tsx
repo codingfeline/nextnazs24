161b1c1ccac683de21a0d07309ff086384f229e7
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-03-07 10:41:28 +0000

    redirect

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import prisma from '@/prisma/client'
 import { redirect } from 'next/navigation'
 
 export default async function Home() {
-  redirect('/journals')
+  redirect('https://google.com')
   // const dataToParse = '<span>**</span>'
   // const chilli = '<GiChiliPepper />'
   const journals = await prisma.journals.findMany()
