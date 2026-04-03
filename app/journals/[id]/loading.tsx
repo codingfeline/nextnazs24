585332b9e22f9b2527a0eb4297fe759b824c4e9b
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-03 16:04:03 +0100

    amend loadings

--- app/journals/[id]/loading.tsx
+++ app/journals/[id]/loading.tsx
@@ -1,17 +1,28 @@
-import { Skeleton } from '@/app/components'
-import { Card } from '@radix-ui/themes'
+import { BackButton, Skeleton } from '@/app/components'
+import ButtonWithComponent from '@/app/components/ButtonLink'
+import MainPage from '@/app/components/MainPage'
+import { Card, Container } from '@radix-ui/themes'
 
 const LoadingJournalDetail = () => {
   return (
-    <div className="max-w-xl ">
-      <Skeleton />
-      <Card className="prose max-w-xl" mt="4">
+    <MainPage>
+      <Container>
+        <ButtonWithComponent
+          href=""
+          classes="ml-3 bg-gray-600 hover:bg-gray-500"
+          Icon={BackButton}
+        >
+          Back to journals
+        </ButtonWithComponent>
         <Skeleton />
-      </Card>
-      <div className="flex justify-end p-1">
-        <Skeleton />
-      </div>
-    </div>
+        <Card className="prose max-w-xl" mt="4">
+          <Skeleton />
+        </Card>
+        <div className="flex justify-end p-1">
+          <Skeleton />
+        </div>
+      </Container>
+    </MainPage>
   )
 }
 
--- app/journals/loading.tsx
+++ app/journals/loading.tsx
@@ -1,44 +1,46 @@
-import { Table } from '@radix-ui/themes'
+import { Container, Table } from '@radix-ui/themes'
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import JournalActions from './journalActions'
 
 const Loading = () => {
-  const journals = [1, 2, 3, 4, 5]
+  const journals = Array.from({ length: 10 }, (_, i) => i + 1)
 
   return (
     <div>
       <JournalActions />
-      <Table.Root variant="surface" className="w-2/3 bg-red-50">
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Topic
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Journal</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Date
-            </Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {journals.map(j => {
-            return (
-              <Table.Row key={j} className="w-full">
-                <Table.Cell className="w-full">
-                  <Skeleton />
-                </Table.Cell>
-                <Table.Cell className="hidden md:table-cell">
-                  <Skeleton />
-                </Table.Cell>
-                <Table.Cell className="hidden md:table-cell">
-                  <Skeleton />
-                </Table.Cell>
-              </Table.Row>
-            )
-          })}
-        </Table.Body>
-      </Table.Root>
+      <Container>
+        <Table.Root variant="surface" className="">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                Topic
+              </Table.ColumnHeaderCell>
+              {/* <Table.ColumnHeaderCell>Journal</Table.ColumnHeaderCell> */}
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                Created
+              </Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {journals.map(j => {
+              return (
+                <Table.Row key={j} className="w-full">
+                  <Table.Cell className="w-full">
+                    <Skeleton />
+                  </Table.Cell>
+                  <Table.Cell className="hidden md:table-cell">
+                    <Skeleton />
+                  </Table.Cell>
+                  <Table.Cell className="hidden md:table-cell">
+                    <Skeleton />
+                  </Table.Cell>
+                </Table.Row>
+              )
+            })}
+          </Table.Body>
+        </Table.Root>
+      </Container>
     </div>
   )
 }
