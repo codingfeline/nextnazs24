0b93637e5f0e381cc411cee430c682d5ad789c46
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-04-03 18:30:14 +0100

    default width

--- app/components/MyContainer.tsx
+++ app/components/MyContainer.tsx
@@ -4,10 +4,10 @@ interface Props {
   size?: string
 }
 
-const MyContainer = ({ header, children, size = 'max-w-md' }: Props) => {
+const MyContainer = ({ header, children, size = 'max-w-lg' }: Props) => {
   return (
     <div
-      className={`mx-auto p-6 bg-gray-100 rounded-md shadow-lg border border-gray-200 text-gray-600 mt-3 ${size} select-none`}
+      className={`MyContainer mx-auto p-6 bg-gray-100 rounded-md shadow-lg border border-gray-200 text-gray-600 mt-3 ${size} select-none`}
     >
       <h4 className="mb-3">{header}</h4>
       {children}
