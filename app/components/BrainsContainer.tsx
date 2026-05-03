a78eabf308acae1bfd4dac8d7d6e7b2266ac43f7
Nazie Sharid <nazie.sh4rid@gmail.com>
2026-05-03 13:00:45 +0100

    replace icon with rotating arrow

--- app/components/BrainsContainer.tsx
+++ app/components/BrainsContainer.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Cpu, Minus, Plus } from '.'
+import { ChevronRight, Cpu } from '.'
 import Reveal from './Reveal'
 
 interface Props {
@@ -18,8 +18,12 @@ const BrainsContainer = ({ children, header = 'the logic' }: Props) => {
           onClick={() => setShow(!show)}
           className={`absolute -top-3 left-1/2 -translate-x-1/2 bg-gray-200 px-4 text-sm font-medium text-gray-700 cursor-pointer hover:text-black transition flex gap-2 items-center rounded-md border border-gray-400 p-1  ${show ? '' : 'border-dashed'}`}
         >
-          <Cpu size={20} title="CPU" />
-          {header} {show ? <Minus size={16} /> : <Plus size={16} />}
+          <Cpu size={20} title="CPU" className={`transition-transform duration-300 ${show ? 'rotate-180' : 'rotate-0'}`} />
+          {header}{' '}
+          <ChevronRight
+            size={16}
+            className={`transition-transform duration-300 ${show ? 'rotate-90' : 'rotate-0'}`}
+          />
         </button>
         <div
           className={`overflow-hidden transition-all duration-300 ease-in-out ${
--- app/components/index.ts
+++ app/components/index.ts
@@ -3,10 +3,10 @@ export { default as Link } from './Link';
 export { default as Skeleton } from './Skeleton';
 export { default as Spinner } from './Spinner';
 
-export { AiOutlineDown as ArrowDown, AiOutlineArrowLeft as ArrowLeft, AiOutlineArrowRight as ArrowRight, AiOutlineUp as ArrowUp, AiOutlineDoubleLeft as DoubleArrowLeft, AiOutlineDoubleRight as DoubleArrowRight } from 'react-icons/ai';
+export { AiOutlineDown as ArrowDown, AiOutlineArrowLeft as ArrowLeft, AiOutlineArrowRight as ArrowRight, AiOutlineUp as ArrowUp, AiOutlineRight as ChevronRight, AiOutlineDoubleLeft as DoubleArrowLeft, AiOutlineDoubleRight as DoubleArrowRight } from 'react-icons/ai';
 export { GiSittingDog as Dog } from 'react-icons/gi';
 export { MdCheckCircle as Check, MdClear as Clear, MdRefresh as Refresh } from "react-icons/md";
-export { PiCpu as Cpu, PiMinus as Minus, PiNotePencil as Pencil, PiPlus as Plus } from "react-icons/pi";
+export { PiCpu as Cpu, PiMinus as Minus, PiNotePencil as Pencil, PiPaperPlaneTilt as Send, PiPlus as Plus } from "react-icons/pi";
 export { RiArrowGoBackFill as BackButton } from "react-icons/ri";
 export { VscCopy as Copy } from 'react-icons/vsc';
 
